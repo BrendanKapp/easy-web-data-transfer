6a4a034cbd99c7ff497036b35ffc32beceac375a
Brendan Kapp <brendankapp@protonmail.com>
2023-10-06 11:44:09 -0500

    Add files

--- app.py
+++ app.py
@@ -3,27 +3,37 @@ import os
 
 app = Flask(__name__)
 
+app.config["UPLOAD_FOLDER"] = "data/files"
+
 def save_text(text, name: str):
     filename = "data/" + name + ".txt"
     if os.path.exists(filename):
         return False, "File " + filename + ".txt already exists!"
     try:
-        with open(filename, 'w') as file:
+        with open(filename, "w") as file:
             file.writelines(text)
     except:
         return False, "Writing data failed!"
     return True, "Success"
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
-        data = request.form['data']
-        name = request.form['name']
+    if request.method == "POST":
+        # check if there is a file upload
+        if "data_file" in request.files:
+            data_file = request.files["data_file"]
+            if data_file.filename != "":
+                filename = os.path.join(app.config["UPLOAD_FOLDER"], data_file.filename)
+                data_file.save(filename)
+                return render_template("index.html", data="", name="", result=("Saved file to: " + filename))
+        # check if there is a text upload
+        data = request.form["data"]
+        name = request.form["name"]
         success, message = save_text(data, name)
         if success == False:
-            return render_template('index.html', data=data, name=name, result="Error: " + message)
-        return render_template('index.html', data='', name='', result=("Saved data to: " + name + ".txt"))
-    return render_template('index.html', data='', name='', result="No Data Saved")
+            return render_template("index.html", data=data, name=name, result="Error: " + message)
+        return render_template("index.html", data="", name="", result=("Saved data to: " + name + ".txt"))
+    return render_template("index.html", data="", name="", result="No Data Saved")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Easy File Transfer
+
+I threw this together in a few minutes to transfer some data from old devices to my PC quickly. Turned out surprisingly helpful.
+
+## Run
+
+> python3 app.py
+
+You may need to change firewall rules to allow port 5000 too.
+
+## Use
+
+Visit `ip:5000` in a web broswer.
+
+For data: type in a filename and the text data then click `Submit`.
+For files: click `Choose File`, select the file, and click `Upload`.
